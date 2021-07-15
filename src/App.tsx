1326a318fd499c7acef4c42dc93fc3ca3cdb1209
litsynp <nocte55is@gmail.com>
2021-07-15 22:52:16 +0900

    feat: enable simple login

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 import "./App.css";
 import Routes from "./components/Routes";
+import Login from "./components/Login/Login";
 
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />;
+  }
+
   return (
     <div className="wrapper">
       <Routes />
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,29 @@
 import { useState } from "react";
+import PropTypes from "prop-types";
+import axios from "axios";
 
-export default function Login() {
+interface CredentialsInterface {
+  username: string;
+  password: string;
+}
+
+async function loginUser(credentials: CredentialsInterface) {
+  return axios
+    .post("http://localhost:8000/api/login", JSON.stringify(credentials), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((res) => {
+      alert("로그인에 성공했습니다!");
+      return res.data;
+    })
+    .catch((err) => {
+      alert("로그인 정보가 틀립니다!");
+    });
+}
+
+export default function Login({ setToken }: any) {
   const [inputs, setInputs] = useState({ username: "", password: "" });
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -9,23 +32,41 @@ export default function Login() {
       ...inputs,
       [name]: value,
     });
+  };
 
-    console.log(value)
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const token = await loginUser({
+      username: inputs.username,
+      password: inputs.password,
+    });
+    setToken(token);
+    console.log(token);
   };
 
   return (
     <div className="max-w-screen-xl mx-auto w-full flex flex-col items-center">
       <div className="flex flex-col items-center p-5 m-5 rounded-md border-2 border-gray-500 w-96">
         <h1 className="pb-5 text-xl font-bold">로그인</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <label className="flex flex-row flex-shrink-0 items-center whitespace-nowrap mb-3 font-bold">
             <p className="w-24">아이디</p>
-            <input className="p-1 rounded-md border-2 border-gray-500" type="text" onChange={onChange} />
+            <input
+              className="p-1 rounded-md border-2 border-gray-500"
+              name="username"
+              type="text"
+              onChange={onChange}
+            />
           </label>
 
           <label className="flex flex-row flex-shrink-0 items-center whitespace-nowrap mb-4 font-bold">
             <p className="w-24">비밀번호</p>
-            <input className="p-1 rounded-md border-2 border-gray-500" type="password" onChange={onChange} />
+            <input
+              className="p-1 rounded-md border-2 border-gray-500"
+              name="password"
+              type="password"
+              onChange={onChange}
+            />
           </label>
 
           <div className="flex justify-center items-center">
@@ -41,3 +82,7 @@ export default function Login() {
     </div>
   );
 }
+
+// Login.propTypes = {
+//   setToken: PropTypes.func.isRequired,
+// };
