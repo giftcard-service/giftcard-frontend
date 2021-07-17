46977eef3def11596ad52580b334e1839de897fb
litsynp <nocte55is@gmail.com>
2021-07-17 16:25:21 +0900

    feat: validate login form

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { withRouter } from "react-router-dom";
+import { useForm } from "react-hook-form";
 import axios from "axios";
 
 import useTokens from "../../utils/useTokens";
@@ -11,12 +12,19 @@ interface CredentialsInterface {
 }
 
 function Login({ history }: { history: any }) {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
   const [inputs, setInputs] = useState({ username: "", password: "" });
   const { setTokens } = useTokens();
 
   async function loginUser(credentials: CredentialsInterface) {
     return axios
-      .post(API_V1_URL + "login", JSON.stringify(credentials), {
+      .post(API_V1_URL + "/login", JSON.stringify(credentials), {
         headers: {
           "Content-Type": "application/json",
         },
@@ -33,19 +41,11 @@ function Login({ history }: { history: any }) {
       });
   }
 
-  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    const tokens = await loginUser({
-      username: inputs.username,
-      password: inputs.password,
+  const onSubmit = (data: { username: string; password: string }) => {
+    const { username, password } = data;
+    const tokens = loginUser({
+      username: username,
+      password: password,
     });
     if (setTokens) {
       setTokens(tokens);
@@ -54,35 +54,47 @@ function Login({ history }: { history: any }) {
 
   return (
     <div className="max-w-screen-xl mx-auto w-full flex flex-col items-center">
-      <div className="flex flex-col items-center p-5 m-5 rounded-md border-2 border-gray-500 w-96">
+      <div className="flex flex-col items-center w-auto md:w-96 p-3 md:p-5 m-5 rounded-md border-2 border-gray-500">
         <h1 className="pb-5 text-xl font-bold">로그인</h1>
-        <form onSubmit={handleSubmit}>
-          <label className="flex flex-row flex-shrink-0 items-center whitespace-nowrap mb-3 font-bold">
-            <p className="w-24">아이디</p>
-            <input
-              className="p-1 rounded-md border-2 border-gray-500"
-              name="username"
-              type="text"
-              onChange={onChange}
-            />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label className="flex flex-col flex-shrink-0 mb-3">
+            <div className="flex flex-row items-center whitespace-nowrap">
+              <p className="w-24 font-bold">아이디</p>
+              <input
+                className="p-1 rounded-md border-2 border-gray-500"
+                type="text"
+                placeholder="아이디"
+                {...register("username", { required: true })}
+              />
+            </div>
+            {errors.username && <div className="text-right text-red-600">*아이디를 입력하세요.</div>}
           </label>
 
-          <label className="flex flex-row flex-shrink-0 items-center whitespace-nowrap mb-4 font-bold">
-            <p className="w-24">비밀번호</p>
-            <input
-              className="p-1 rounded-md border-2 border-gray-500"
-              name="password"
-              type="password"
-              onChange={onChange}
-            />
+          <label className="flex flex-col flex-shrink-0 mb-3">
+            <div className="flex flex-row items-center whitespace-nowrap">
+              <p className="w-24 font-bold">비밀번호</p>
+              <input
+                className="p-1 rounded-md border-2 border-gray-500"
+                type="password"
+                placeholder="비밀번호"
+                {...register("password", { required: true })}
+              />
+            </div>
+            {errors.password && <div className="text-right text-red-600">*비밀번호를 입력하세요.</div>}
           </label>
 
           <div className="flex justify-center items-center">
             <button className="rounded-md bg-gray-600 text-white font-bold p-2 mr-2" type="submit">
-              회원가입
-            </button>
-            <button className="rounded-md bg-gray-600 text-white font-bold p-2" type="submit">
               로그인
+            </button>
+            <button
+              className="rounded-md bg-gray-600 text-white font-bold p-2"
+              onClick={(e) => {
+                e.preventDefault();
+                history.push("/signup");
+              }}
+            >
+              회원가입
             </button>
           </div>
         </form>
