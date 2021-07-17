a5f836415696b830dc2a05d64a84cd95c1a1675d
litsynp <nocte55is@gmail.com>
2021-07-17 10:56:28 +0900

    feat: add test giftcard list component

---
+++ src/components/Giftcards/GiftcardList.tsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+
+function GiftcardList() {
+  const [giftcards, setGiftcards] = useState({
+    count: 0,
+    result: [
+      { id: 1, store: { id: 1, name: "현대백화점" } },
+      { id: 2, store: { id: 4, name: "신세계백화점" } },
+      { id: 3, store: { id: 2, name: "롯데시네마" } },
+      { id: 4, store: { id: 3, name: "롯데백화점" } },
+      { id: 5, store: { id: 4, name: "신세계백화점" } },
+    ],
+  });
+
+  return (
+    <div className="flex flex-col w-full items-center mx-auto">
+      <h1 className="pb-1 text-xl font-bold mb-3">내 상품권</h1>
+      <div className="flex flex-col">
+        {giftcards.result.map((giftcard) => (
+          <div className="flex flex-col items-center p-2 mb-3 rounded-md border-2 border-gray-500 w-full cursor-pointer">
+            <div className="text-xl font-bold">상품권 ID: {giftcard.id}</div>
+            <div className="flex flex-col w-full p-2 rounded-md border-2 border-gray-500">
+              <div className="text-center font-bold">매장 정보</div>
+              <div className="flex flex-row text-sm">
+                <div className="font-bold">매장 ID:</div>
+                <div>{giftcard.store.id}</div>
+              </div>
+              <div className="flex flex-row text-sm">
+                <div className="font-bold">매장 이름:</div>
+                <div>{giftcard.store.name}</div>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default GiftcardList;
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,9 +1,20 @@
 import { withRouter } from "react-router-dom";
 
+import useTokens from "../../utils/useTokens";
+import GiftcardList from "../Giftcards/GiftcardList";
+
 function Home({ history }: { history: any }) {
+  const { isLoggedIn } = useTokens();
+
   return (
     <div className="max-w-screen-xl mx-auto w-full flex flex-col items-center">
       <div className="p-5 text-3xl font-bold">상품권 관리 서비스</div>
+
+      {isLoggedIn && (
+        <div>
+          <GiftcardList />
+        </div>
+      )}
     </div>
   );
 }
