25368ffbc2369163c7fd2d67655021034dda66d3
litsynp <nocte55is@gmail.com>
2021-07-20 23:18:30 +0900

    refactor: separate giftcard item component from list

---
+++ src/components/Giftcard/GiftcardItem.tsx
@@ -0,0 +1,27 @@
+interface GiftcardItemPropsInterface {
+  giftcard: { id: string; store: { id: string; name: string } };
+}
+
+function GiftcardItem({ giftcard }: GiftcardItemPropsInterface) {
+  return (
+    <div className="flex flex-col items-center p-2 mb-5 rounded-md border-2 border-gray-500 w-full cursor-pointer">
+      <div className="flex flex-row w-full items-center">
+        <div className="font-bold mr-1">ID:</div>
+        <div className="w-full truncate">{giftcard.id}</div>
+      </div>
+      <div className="flex flex-col w-full p-2 rounded-md border-2 border-gray-500">
+        <div className="text-center font-bold">매장 정보</div>
+        <div className="flex flex-row w-full text-sm">
+          <div className="w-1/3 font-bold mr-1">매장 ID:</div>
+          <div className="w-full text-right truncate">{giftcard.store.id}</div>
+        </div>
+        <div className="flex flex-row w-full text-sm">
+          <div className="w-1/3 font-bold mr-1">매장 이름:</div>
+          <div className="w-full text-right truncate">{giftcard.store.name}</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default GiftcardItem;
--- src/components/Giftcard/GiftcardList.tsx
+++ src/components/Giftcard/GiftcardList.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 import { findGiftcardList } from "../../services/GiftcardService";
 import useTokens from "../../utils/useTokens";
+import GiftcardItem from "./GiftcardItem";
 
 function GiftcardList() {
   const { tokens } = useTokens();
@@ -20,23 +21,7 @@ function GiftcardList() {
       <h1 className="pb-1 text-xl font-bold mb-2">내 상품권</h1>
       <div className="flex flex-col w-full md:w-1/3">
         {giftcardList.items.map((giftcard) => (
-          <div className="flex flex-col items-center p-2 mb-5 rounded-md border-2 border-gray-500 w-full cursor-pointer">
-            <div className="flex flex-row w-full items-center">
-              <div className="font-bold mr-1">ID:</div>
-              <div className="w-full truncate">{giftcard.id}</div>
-            </div>
-            <div className="flex flex-col w-full p-2 rounded-md border-2 border-gray-500">
-              <div className="text-center font-bold">매장 정보</div>
-              <div className="flex flex-row w-full text-sm">
-                <div className="w-1/3 font-bold mr-1">매장 ID:</div>
-                <div className="w-full text-right truncate">{giftcard.store.id}</div>
-              </div>
-              <div className="flex flex-row w-full text-sm">
-                <div className="w-1/3 font-bold mr-1">매장 이름:</div>
-                <div className="w-full text-right truncate">{giftcard.store.name}</div>
-              </div>
-            </div>
-          </div>
+          <GiftcardItem giftcard={giftcard} />
         ))}
       </div>
     </div>
